e9303a2a46650b90a9fca43255ac52cb6aece070
Biswajit Bera <BS.BISWAJIT2005@gmil.com>
2025-07-18 16:28:32 +0530

    Fix case-sensitive import path for Login component

--- fish/src/App.jsx
+++ fish/src/App.jsx
@@ -1,9 +1,10 @@
 import { useState } from 'react'
 import './App.css'
 
-import Login from './Components/login'
+
 import Header from './Components/Header'
 import Footer from './Components/Footer'
+import Login from './Components/login'
 
 function App() {
 
