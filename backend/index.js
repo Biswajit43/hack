ce60afdafc35fe3d9cbc8c8289ecf32f93c48325
Biswajit Bera <BS.BISWAJIT2005@gmil.com>
2025-07-18 16:44:55 +0530

    new

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,26 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 const cors = require('cors');
+const cors = require('cors');
+
+const allowedOrigins = [
+    'http://localhost:5173',
+    'https://facebook-remote-aptitude-test.onrender.com'
+];
+
 app.use(cors({
-        origin: ['http://localhost:5173', 'https://facebook-remote-aptitude-test.onrender.com/'],  // during development
-    credentials: true
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    credentials: true,
+    methods: ['GET', 'POST', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
 }));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 const usermodel = require('./model/usermodel')
